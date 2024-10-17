56bfd5baea6100aef2a4963510dae4b2056648a2
pavan-klbg <allurpavan111@gmail.com>
2024-10-17 19:03:16 +0530

    added serializable for redis implementation

--- src/main/java/com/employee/data/entity/Employee.java
+++ src/main/java/com/employee/data/entity/Employee.java
@@ -6,6 +6,8 @@ import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
+import java.io.Serial;
+import java.io.Serializable;
 import java.util.Date;
 import java.util.List;
 
@@ -14,7 +16,10 @@ import java.util.List;
 @NoArgsConstructor
 
 @Entity
-public class Employee {
+public class Employee implements Serializable {
+
+    @Serial
+    private static final long serialVersionUID = 1L;
 
     @Id
     @GeneratedValue(strategy = GenerationType.AUTO)
