d4a54b2508bd5d66031e683a104d56b578c0865f
pavan-klbg <allurpavan111@gmail.com>
2024-10-17 19:04:14 +0530

    aligned identation of class

--- src/main/java/com/employee/data/controller/EmployeeController.java
+++ src/main/java/com/employee/data/controller/EmployeeController.java
@@ -4,42 +4,42 @@ import com.employee.data.entity.Employee;
 import com.employee.data.service.EmployeeService;
 import jakarta.validation.Valid;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.cache.annotation.Cacheable;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
-
-@RestController
 @Validated
+@RestController
 public class EmployeeController {
     @Autowired
-  EmployeeService employeeService;
+    EmployeeService employeeService;
 
 
     // method to save employee
     @PostMapping("/employee")
-public ResponseEntity<Employee> saveEmployeeData(@Valid @RequestBody Employee employee){
-return  new ResponseEntity<>(employeeService.saveEmployee(employee),HttpStatus.CREATED);
-}
+    public ResponseEntity<Employee> saveEmployeeData(@Valid @RequestBody Employee employee) {
+        return new ResponseEntity<>(employeeService.saveEmployee(employee), HttpStatus.CREATED);
+    }
 
-// get all employees
+    // get all employees
     @GetMapping("/employee")
-    public ResponseEntity<List<Employee>> getAllEmployees(){
-        return new ResponseEntity<>(employeeService.getAllEmployees(),HttpStatus.OK);
+    public ResponseEntity<List<Employee>> getAllEmployees() {
+        return new ResponseEntity<>(employeeService.getAllEmployees(), HttpStatus.OK);
     }
 
     // get By emp id
     @GetMapping("/employee/{id}")
-    public ResponseEntity<Employee> getById(@PathVariable("id") Integer id){
-return  new ResponseEntity<>(employeeService.getEmployeeById(id),HttpStatus.OK);
+    public ResponseEntity<Employee> getById(@PathVariable("id") Integer id) {
+        return new ResponseEntity<>(employeeService.getEmployeeById(id), HttpStatus.OK);
     }
 
     // update employee by passing id
     @PutMapping("employee/{id}")
-    public ResponseEntity<Employee> updateEmployeeData(@Valid @PathVariable("id") Integer id, @RequestBody Employee employee){
-        return new ResponseEntity<>(employeeService.updateEmployee(id, employee),HttpStatus.OK);
+    public ResponseEntity<Employee> updateEmployeeData(@Valid @PathVariable("id") Integer id, @RequestBody Employee employee) {
+        return new ResponseEntity<>(employeeService.updateEmployee(id, employee), HttpStatus.OK);
     }
 
     // delete all employee
